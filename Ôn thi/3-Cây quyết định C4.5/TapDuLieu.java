720c6d22d444aab328c35d9057ee77736eb0f9ab
Azoomdh <94795620+Azoomdh@users.noreply.github.com>
2026-05-16 00:39:27 +0700

    cây quyết định v1

---
+++ Ôn thi/3-Cây quyết định C4.5/TapDuLieu.java
@@ -0,0 +1,43 @@
+class TapDuLieu {
+	
+	// tapDuLieu.get() trả về một TapDuLieu
+	public TapDuLieu get(String tenCot, Object giaTri) {
+		// …
+	}
+
+	public ArrayList<Object> getTapGiaTri(string tenCot){
+		// …
+	}
+
+	public Integer getLength(){
+		// …
+	}
+
+	// ***************************
+
+	public double getGiniSplit(String tenCot){
+		// ...
+	}
+
+	public double getGini(String tenCot){
+		// …
+	}
+
+	// ***************************
+
+	public double getEntropy(){
+		// ...
+	}
+
+	public double getInfoGain(String tenCot){
+		// ...
+	}
+
+	public double getSplitInfo(String tenCot){
+		// ...
+	}
+
+	public double getGainRatio(String tenCot){
+		// ...
+	}
+}
